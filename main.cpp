58045348fb48d4a090da95c3487a64e524470747
eton <bbaodi@ace-cardio.com>
2023-12-12 18:13:50 +0800

    support draw ellipse using two points

--- main.cpp
+++ main.cpp
@@ -24,10 +24,109 @@ int main(int argc, char *argv[])
 */
 //#include <QGui/QGuiApplication>
 #include <QDebug>
+#include <QLineF>
 #include <QPainter>
 #include <QPainterPath>
+#include <QPoint>
 #include <QQuickPaintedItem>
 #include <QTransform>
+#include <math.h>
+
+class EllipseShape {
+public:
+  EllipseShape() : m_ready(false), m_fillPtNum(0) {}
+  bool getAngleWithXAxix(qreal &angleDegree) {
+    angleDegree = 0.f;
+    if (false == m_ready) {
+      return false;
+    }
+    const QLineF &slip = m_longDiameter;
+    QPointF pt1 = slip.p1();
+    QPointF pt2 = slip.p2();
+    /* if (pt2.x() < pt1.x()) {
+          pt2 = pt1;
+          pt1 = slip.p2();
+        }*/
+    float deltaX = pt2.x() - pt1.x();
+    float deltaY = -(pt2.y() - pt1.y());
+
+    angleDegree = std::atan2(deltaY, deltaX);
+    angleDegree = qRadiansToDegrees(angleDegree);
+    qDebug() << "angle=" << angleDegree << ", line=" << m_longDiameter << ",qAngle=" << m_longDiameter.angle();
+    return true;
+  }
+  qreal getAngle2() { return -1.f * m_longDiameter.angle(); }
+  void addPoint(QPoint pt) {
+    switch (m_fillPtNum) {
+    case 0: {
+      m_longDiameter.setP1(pt);
+      m_fillPtNum++;
+      break;
+    }
+    default:
+    case 1: {
+      m_longDiameter.setP2(pt);
+      m_fillPtNum++;
+      m_ready = true;
+      setShortDiameter();
+      break;
+    }
+    }
+  }
+  void addPoint(QPointF pt) { return addPoint(pt.toPoint()); }
+  void setShortDiameter() {
+    // QPoint center = m_longDiameter.center();
+    m_shortDiameterLength = m_longDiameter.length();
+  }
+  bool isReady() const { return m_ready; }
+  QLineF getLongDia() const { return m_longDiameter; }
+
+  QRectF getRect() {
+    QRectF rect;
+    rect.setWidth(m_longDiameter.length());
+    rect.setHeight(rect.width() * 0.5);
+    rect.moveCenter(m_longDiameter.center());
+
+    return rect;
+  }
+
+private:
+  QLineF m_longDiameter;
+  qreal m_shortDiameterLength;
+  bool m_ready;
+  int m_fillPtNum;
+};
+
+//!
+//! \brief transformEllipse rotate a ellipse defined by rect with angleA;
+//! \param bbox
+//! \param angleA
+//! \param rpolygon
+//! \return
+//!
+int transformEllipse(QRectF bbox, qreal angleA, QPolygonF &rpolygon) {
+  QPainterPath ellipsePath;
+  ellipsePath.addEllipse(bbox);
+
+  QTransform transform;
+  QTransform trabefore, traBack;
+
+  qreal centerX = bbox.x() + bbox.width() / 2;
+  qreal centerY = bbox.y() + bbox.height() / 2;
+
+  trabefore.translate(-centerX, -centerY);
+  transform.rotate(angleA, Qt::ZAxis, 0);
+
+  QPolygonF polygon = ellipsePath.toFillPolygon();
+  polygon = trabefore.map(polygon); // 1. tras to 00
+  polygon = transform.map(polygon); // 2. rotate
+
+  traBack.translate(centerX, centerY); // 3. tras back
+  polygon = traBack.map(polygon);
+  rpolygon = polygon;
+
+  return 0;
+}
 
 class EllipseWidget : public QQuickPaintedItem {
 public:
@@ -38,7 +137,14 @@ public:
   }
 
 protected:
-  virtual void mousePressEvent(QMouseEvent *event) override { qDebug() << ("press~\n"); }
+  virtual void mousePressEvent(QMouseEvent *event) override {
+    qDebug() << ("press~\n");
+    m_elli.addPoint(event->position());
+    qreal angle;
+    bool ret = m_elli.getAngleWithXAxix(angle);
+    qDebug() << "ret=" << ret << ", angle=" << angle;
+    update();
+  }
   virtual void mouseMoveEvent(QMouseEvent *event) override { printf("mouse movex=%d\n", event->position().x()); }
   virtual void mouseReleaseEvent(QMouseEvent *event) override { printf("mouse rx=%d\n", event->position().x()); }
   virtual void mouseDoubleClickEvent(QMouseEvent *event) override {
@@ -55,32 +161,19 @@ protected:
     if (360 == m_angleA && x > 0) {
       m_angleA = 0;
     }
-    qDebug() << "x=" << x << ", A=" << m_angleA;
+    qDebug() << "wheel Event:x=" << x << ", A=" << m_angleA;
     update();
   }
 
 protected:
   void paint(QPainter *painter) {
-    printf("geometry:x=%f,w=%f", x(), width());
     QRectF bbox(110, 110, 100, 60);
-    QPainterPath ellipsePath;
-    ellipsePath.addEllipse(bbox);
-
-    QTransform transform;
-    QTransform trabefore, traBack;
-
-    qreal centerX = bbox.x() + bbox.width() / 2;
-    qreal centerY = bbox.y() + bbox.height() / 2;
-
-    trabefore.translate(-centerX, -centerY);
-    transform.rotate(m_angleA, Qt::ZAxis, 0);
-
-    QPolygonF polygon = ellipsePath.toFillPolygon();
-    polygon = trabefore.map(polygon); // 1. tras to 00
-    polygon = transform.map(polygon); // 2. rotate
-
-    traBack.translate(centerX, centerY); // 3. tras back
-    polygon = traBack.map(polygon);
+    if (false == m_elli.isReady()) {
+      return;
+    }
+    bbox = m_elli.getRect();
+    QPolygonF polygon;
+    transformEllipse(bbox, m_elli.getAngle2(), polygon);
 
     QString msg = QString("Angle={%1}Degree").arg(m_angleA);
 
@@ -93,8 +186,13 @@ protected:
     painter->setPen(myPen);
     painter->drawRect(bbox);
     painter->drawText(70, 70, msg);
+
+    if (m_elli.isReady()) {
+      painter->drawLine(m_elli.getLongDia());
+    }
   }
   int m_angleA = 40;
+  EllipseShape m_elli;
 };
 
 int main(int argc, char *argv[]) {
